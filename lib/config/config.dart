24b4bff26b01f7edaf433c2d53f437d73b5307c5
Gio <seturidee@mail.ru>
2020-05-21 22:09:59 +0300

    lala

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:device_id/device_id.dart';
+import 'package:food_delivery/data/data.dart';
+import 'package:food_delivery/models/AuthCode.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class NecessaryDataForAuth{
  String device_id;
@@ -18,6 +24,9 @@ class NecessaryDataForAuth{
    String phone_number = prefs.getString('phone_number');
    String refresh_token = prefs.getString('refresh_token');
    NecessaryDataForAuth result = new NecessaryDataForAuth(device_id: device_id, phone_number: phone_number, refresh_token: refresh_token);
+   if(!(await refreshToken(refresh_token))){
+     result.refresh_token = null;
+   }
    return result;
  }
 
@@ -27,6 +36,23 @@ class NecessaryDataForAuth{
    prefs.setString('phone_number', phone_number);
    prefs.setString('refresh_token',refresh_token);
    NecessaryDataForAuth result = new NecessaryDataForAuth(device_id: device_id, phone_number: phone_number, refresh_token: refresh_token);
+   return result;
+ }
+
+ static Future<bool> refreshToken(String refresh_token) async {
+   bool result = false;
+   var url = 'https://client.apis.stage.faem.pro/api/v2/auth/refresh';
+   var response = await http.post(url, body: jsonEncode({"refresh": refresh_token}),
+       headers: <String, String>{
+         'Content-Type': 'application/json; charset=UTF-8',
+       });
+   if (response.statusCode == 200) {
+     result = true;
+     var jsonResponse = convert.jsonDecode(response.body);
+     authCodeData = AuthCodeData.fromJson(jsonResponse);
+   } else {
+     print('Request failed with status: ${response.statusCode}.');
+   }
    return result;
  }
 }
--- lib/screens/device_id_screen.dart
+++ lib/screens/device_id_screen.dart
@@ -1,13 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:food_delivery/config/config.dart';
 import 'package:food_delivery/data/data.dart';
-import 'package:food_delivery/models/AuthCode.dart';
 import 'package:food_delivery/screens/auth_screen.dart';
 import 'package:food_delivery/screens/home_screen.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 
 class DeviceIdScreen extends StatelessWidget {
 
@@ -22,7 +18,6 @@ class DeviceIdScreen extends StatelessWidget {
           if(necessaryDataForAuth.refresh_token == null){
             return AuthScreen();
           }
-          refreshToken(necessaryDataForAuth.refresh_token);
           print(necessaryDataForAuth.refresh_token);
           return HomeScreen();
         }else{
@@ -32,20 +27,5 @@ class DeviceIdScreen extends StatelessWidget {
         }
       },
     );
-  }
-
-  Future refreshToken(String refresh_token) async {
-    var url = 'https://client.apis.stage.faem.pro/api/v2/auth/refresh';
-    var response = await http.post(url, body: jsonEncode({"refresh": refresh_token}),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        });
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      authCodeData = AuthCodeData.fromJson(jsonResponse);
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
-    print(response.body);
   }
 }
